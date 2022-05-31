8996c19a723fcd797b6ff72a36f3ea53d41b14d5
yatharthyadu <91853007+yatharthyadu@users.noreply.github.com>
2022-05-31 06:50:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './components/Card';
+
+function App() {
+  
+  return (
+    <div>
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
